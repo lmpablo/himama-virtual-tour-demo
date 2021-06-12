f6a17f3f8b68229ecbcadebd6cfb1744abfb8665
Luis Pablo <luismpablo@gmail.com>
2021-06-11 23:43:31 -0400

    fix paths

---
+++ _nuxt/static/1623469375/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhimama-virtual-tour-demo\u002F_nuxt\u002Fstatic\u002F1623469375",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fhimama-virtual-tour-demo\u002F",assetsPath:"\u002Fhimama-virtual-tour-demo\u002F_nuxt\u002F",cdnURL:a}}}}(null));
